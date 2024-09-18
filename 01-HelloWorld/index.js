1e3e71714b0814c900d408c58c19a5a69212f16c
Alejandro Ramirez P <alejorp195@hotmail.com>
2024-09-17 20:40:46 -0500

    add taskt to readme in 00-Nodejs-introduction

--- 01-HelloWorld/index.js
+++ 01-HelloWorld/index.js
@@ -1,10 +1,14 @@
 console.log('Hello World!');
 
-// const { saludar } = require('./saludarCM');
+// const { saludar } = require('./saludarCM'); // sincrono
 
-// console.log(saludar('Juan'));  
+// console.log(saludar('Juan'));   //CommonJS
 
 
 // import { saludar } from "./saludarESM.js";
 
 // console.log(saludar('Juan')); 
+
+import { saludar } from './saludarESM.js' // Asincronó
+
+console.log(saludar('Juanes'));
